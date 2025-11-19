aed2c1b2ff0513f296d012d2a2e991c8f5bda786
Aubrey Nichole L. Nastor <nicollonastor@gmail.com>
2025-11-19 22:20:05 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        //Constructor = A special method to initialize objects
+        //              You can pass arguments to a constructor
+        //              and set up initial values
+        //This is a practice or a new learning
+
+
+        Student student1 = new Student("Aubrey", 23, 2.5);
+        Student student2 = new Student("Nichole", 24, 1.5);
+
+        System.out.println(student1.name);
+        System.out.println(student2.name);
+
+    }
+}
